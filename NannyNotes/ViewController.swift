053ce04bdb75800ae81a426bc6b1ad152f7a9ffe
Suzie <reorsusie@gmail.com>
2023-04-13 11:02:25 -0700

    Initial Commit

---
+++ NannyNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NannyNotes
+//
+//  Created by Suzie on 4/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
